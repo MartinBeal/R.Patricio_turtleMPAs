6d4abc2674776a52f57f2af8ccd3c33438c0c634
Bealhammar <mbeal@ispa.pt>
2021-10-11 09:55:40 +0100

    study site map: larger text

--- scripts/map_studyarea.R
+++ scripts/map_studyarea.R
@@ -12,7 +12,8 @@ babr <- st_as_sf(babr)
 
 ## load bathymetry data ##
 # depth <- raster("data/geodata/GEBCO_2020_W Africa/gebco_2020_n20.9_s10.1_w-18.9_e-14.8.tif")
-depth <- raster("data/geodata/ETOPO1.tiff")
+# depth <- raster("data/geodata/ETOPO1.tiff")
+depth     <- raster("data/geodata/ETOPO1_patchedw_bathyrastPNBA.tif")
 
 # nodepthzone <- raster::shapefile("data/geodata/Bathy_BA/no_depth_info_zone.shp")
 # nodepthzone_sf  <- st_as_sf(nodepthzone)
@@ -34,8 +35,8 @@ poilao <- st_as_sf(poilao, coords = c("Longitude", "Latitude"), crs = 4326, agr 
 xtnt <- extent(depth)
 
 # plot(depth2)
-cntr_lvls <- c(-24)
-# cntr_lvls <- c(-12)
+# cntr_lvls <- c(-24)
+cntr_lvls <- c(-13)
 # cntr_lvls <- c(-50, -100, -500, -1000, -2000, -3000 -4000, -5000)
 cntrs <- st_as_sf(rasterToContour(depth, levels=cntr_lvls))
 
@@ -46,39 +47,42 @@ gplot(depth) +
   geom_sf(data=cntrs, inherit.aes = FALSE, color="grey20", size=.35, fill=NA) +
   # geom_sf(data=babr, inherit.aes = FALSE, aes(color="grey1"), linetype="twodash",  size=.75, fill=NA, show.legend = T) +
   # geom_sf(data=mpas, inherit.aes = FALSE, aes(color="black"), size=1.25, fill=NA) +
-  geom_sf(data = land, inherit.aes = FALSE, fill="grey65", colour="grey40") +
+  geom_sf(
+    data = land, inherit.aes = FALSE, fill="grey65", colour="grey40") +
   geom_sf(
     data=babr, inherit.aes = FALSE, aes(linetype="dashed"), color="black",  size=1, fill=NA, show.legend = "line") +
   geom_sf(
     data=mpas, inherit.aes = FALSE, aes(linetype="solid"), color="black", size=1, fill=NA, show.legend = "line") +
   geom_sf(
-    data = poilao, inherit.aes = FALSE, fill="gold1", aes(color="Poilão"), size = 5.5, stroke=1.5, shape=23) +
+    data = poilao, inherit.aes = FALSE, fill="gold1", color="black", 
+    size = 5.5, stroke=1.5, shape=23,
+    show.legend = FALSE) +
   coord_sf(
     xlim = c(xtnt@xmin, xtnt@xmax), ylim = c(xtnt@ymin, xtnt@ymax), expand = F) +
   scale_x_continuous(breaks = seq(-19, -15, by = 1)) +
-  scale_color_manual(
-    name = NULL,
-    values = c("Poilão"="black")) +
+  # scale_color_manual(
+  #   name = NULL,
+  #   values = c("Poilão"="black")) +
   scale_linetype_identity(
     name = NULL,
     breaks = c("solid", "dashed"),
     labels = c("MPA", "BABR"),
     guide = "legend") +
   theme(
-    legend.title = element_text(size=11),
-    legend.position = c(0.85, .5),
+    legend.title = element_text(size=16),
+    legend.text  = element_text(size=16),
+    legend.position = c(0.82, .5),
     legend.background = element_rect(fill=NA, colour=NA),
     legend.key = element_rect(fill=NA, colour=NA),
     # legend.key.width = unit(2.5,"line"),
     panel.background=element_rect(fill=NA, colour="black"),
     panel.border = element_rect(colour = "black", fill=NA, size=1),
-    axis.text=element_text(size=12, color="black"),
-    axis.title=element_text(size=16)) +
+    axis.text=element_text(size=16, color="black")) +
   ylab("") +
   xlab("") + 
-  labs(fill = "Depth (m)") + guides(linetype = guide_legend(keywidth = unit(3,"line"))) +
+  labs(fill = "Depth (m)") + guides(linetype = guide_legend(keywidth = unit(2.5,"line"))) +
   ggspatial::annotation_scale(style="ticks", height=unit(0.5, "cm"), text_cex = 1)
 
 ## SAVE ## 
-ggsave("figures/smallscale_WAfrica_24m_2xx.png", width = 6, height=13 )
+ggsave("figures/study_area/smallscale_WAfrica_13m_2xx.png", width = 5, height=12 )
 
